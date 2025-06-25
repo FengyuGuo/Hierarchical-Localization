24e7458b894cb212bdcf1d60965121ed29942f81
Guo, Fengyu <fyguo@telenav.cn>
2025-06-25 20:37:31 +0800

    test megaloc and spsg

--- test_netvlad.py
+++ test_netvlad.py
@@ -1,9 +1,9 @@
 from hloc.extractors.netvlad import NetVLAD
-
+from hloc.extractors.megaloc import MegaPlaces
 import cv2
 import numpy as np
 import torch
-test_img_path = '/home/guo/l.jpg'
+test_img_path = '/home/guo/l.png'
 
 img=cv2.imread(test_img_path)
 
@@ -14,14 +14,23 @@ img=img.transpose([2, 0, 1])
 img=np.expand_dims(img, axis=0)
 img = img/255.0
 print(img.shape, img.dtype)
-img_tensor = torch.tensor(img.astype('float32'))
+img_tensor = torch.tensor(img.astype('float32')).to('cuda')
 data = {'image':img_tensor}
 netvlad_conf = {
   'model_name': 'VGG16-NetVLAD-Pitts30K',
   'whiten': True
 }
-netvlad = NetVLAD(netvlad_conf)
+netvlad = NetVLAD(netvlad_conf).eval().to('cuda')
 desc = netvlad(data)
 
+
+
 print(desc)
+print(desc['global_descriptor'].dtype)
+print(desc['global_descriptor'].shape)
+
+mega = MegaPlaces(MegaPlaces.default_conf).eval().to('cuda')
+
+desc = mega(data)
+print(desc['global_descriptor'].dtype)
 print(desc['global_descriptor'].shape)
--- visualize_netvlad_result.py
+++ visualize_netvlad_result.py
@@ -4,9 +4,63 @@ import random
 import numpy as np
 import os.path as osp
 import cv2
-result_path = '/media/guo/fs/Hierarchical-Localization/night_netvlad_top_10.txt'
-query_root = '/media/guo/fs/Hierarchical-Localization/office_night_query'
-train_root = '/media/guo/fs/Hierarchical-Localization/office'
+from hloc.extractors import superpoint
+from hloc.matchers import superglue
+import torch
+import argparse
+
+from camera.camera_model import EquiDistCamera
+from dataset.mynteye_left import load_mynteye
+
+
+cam_param = load_mynteye()
+cam_model = EquiDistCamera(np.array(cam_param['cam_mat'],dtype='float32'), np.array(cam_param['distortion'], dtype='float32'), np.array(cam_param['img_size_wh']))
+
+# example:
+# python visualize_netvlad_result.py --result_txt /media/guo/fs/Hierarchical-Localization/megaloc_top20.txt --query_img_path /media/guo/fs/Hierarchical-Localization/office_night_query/ --train_img_path /media/guo/fs/Hierarchical-Localization/office
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+  '--result_txt',
+  type=str,
+  required=True,
+  help='path to the result with match pairs and scores'
+)
+
+parser.add_argument(
+  '--query_img_path',
+  type=str,
+  required=True,
+  help='root dir of query images'
+)
+
+parser.add_argument(
+  '--train_img_path',
+  type=str,
+  required=True,
+  help='root dir of train images'
+)
+
+parser.add_argument(
+  '--try_match',
+  default=False,
+  action='store_true',
+  help='extract and match feature points'
+)
+
+args = parser.parse_args()
+
+# result_path = '/media/guo/fs/Hierarchical-Localization/night_netvlad_top_10.txt'
+# query_root = '/media/guo/fs/Hierarchical-Localization/office_night_query'
+# train_root = '/media/guo/fs/Hierarchical-Localization/office'
+
+result_path = args.result_txt
+query_root = args.query_img_path
+train_root = args.train_img_path
+
+sp = superpoint.SuperPoint(superpoint.SuperPoint.default_conf).eval().to('cuda')
+sg = superglue.SuperGlue(superglue.SuperGlue.default_conf).eval().to('cuda')
 
 df = pd.read_csv(result_path, delimiter=' ', names=['query', 'train', 'score'])
 
@@ -14,8 +68,8 @@ print(df.head(10))
 
 query_imgs = pd.unique(df['query'].sort_values())
 # print(query_imgs)
-for i in range(1000):
-  # q=random.choice(query_imgs)
+for i in range(1994, len(query_imgs)):
+  print('index: ', i)
   q=query_imgs[i]
   print('q:', q)
   t0=df[df['query'] == q]['train'].iloc[0]
@@ -24,44 +78,87 @@ for i in range(1000):
   score = df[df['query'] == q]['score'].iloc[0]
   score1 = df[df['query'] == q]['score'].iloc[1]
   score2 = df[df['query'] == q]['score'].iloc[2]
-  # print(score)
   print('t_max:', t0)
-  # print(t['train'])
-  # print(t['train'].iloc[0])
-
   q_path=osp.join(query_root, q)
   t0_path=osp.join(train_root, t0)
 
-  q_img = cv2.imread(q_path)
-  t_img = cv2.imread(t0_path)
+  q_img = cv2.imread(q_path, cv2.IMREAD_GRAYSCALE)
+  t_img = cv2.imread(t0_path, cv2.IMREAD_GRAYSCALE)
 
   t1_path=osp.join(train_root, t1)
-  t1_img = cv2.imread(t1_path)
+  t1_img = cv2.imread(t1_path, cv2.IMREAD_GRAYSCALE)
   t2_path=osp.join(train_root, t2)
-  t2_img = cv2.imread(t2_path)
+  t2_img = cv2.imread(t2_path, cv2.IMREAD_GRAYSCALE)
 
   viz = np.hstack((q_img, t_img))
 
   bt = np.hstack((t1_img, t2_img))
 
   viz = np.vstack((viz, bt))
+  viz = cv2.cvtColor(viz, cv2.COLOR_GRAY2BGR)
 
+  if args.try_match:
+    q_tensor = torch.from_numpy(q_img).float() / 255.0
+    q_tensor = q_tensor.unsqueeze(0).unsqueeze(0)
+    # print(img_tensor.shape)
+    q_desc = sp({"image": q_tensor.to('cuda', non_blocking=True)})
+    # print(q_desc['keypoints'][0])
+    # print(q_desc.keys())
+    t_tensor = torch.from_numpy(t_img).float() / 255.0
+    t_tensor = t_tensor.unsqueeze(0).unsqueeze(0)
+    t_desc = sp({"image": t_tensor.to('cuda', non_blocking=True)})
+    kps0 = q_desc['keypoints'][0].cpu().numpy()
+    kps1 = t_desc['keypoints'][0].cpu().numpy()
+    match_input = {
+      'image0': q_tensor,
+      'keypoints0': q_desc['keypoints'][0].unsqueeze(0),
+      'scores0': q_desc['scores'][0].unsqueeze(0),
+      'descriptors0': q_desc['descriptors'][0].unsqueeze(0),
+      'image1': t_tensor,
+      'keypoints1': t_desc['keypoints'][0].unsqueeze(0),
+      'scores1': t_desc['scores'][0].unsqueeze(0),
+      'descriptors1': t_desc['descriptors'][0].unsqueeze(0)
+    }
+    match = sg(match_input)
+    mt0 = match['matches0'].cpu().numpy()[0]
+    # print(mt0)
+    pts0 = []
+    pts1 = []
+    for idx1, idx2 in enumerate(mt0):
+      if idx2 == -1:
+        continue
+      pts0.append(kps0[idx1])
+      pts1.append(kps1[idx2])
+    pts0 = np.array(pts0, dtype='float32').reshape(-1, 1, 2)
+    pts1 = np.array(pts1, dtype='float32').reshape(-1, 1, 2)
+    print(pts0.shape)
+    if pts0.shape[0] > 0:
+      pts0_n = cam_model.undistort_points(pts0)
+      pts1_n = cam_model.undistort_points(pts1)
+      F, mask = cv2.findFundamentalMat(pts0_n, pts1_n, cv2.FM_RANSAC, ransacReprojThreshold=1.0, confidence=0.99, maxIters = 200)
+      good = 0
+      mask = np.squeeze(mask)
+      print(mask.shape)
+      good_match = np.count_nonzero(mask)
+      if good_match > 0:
+        for i, m in enumerate(mask.tolist()):
+          if m == 0:
+            continue
+          good += 1
+          pt0 = pts0.astype('int')[i][0]
+          pt1 = pts1.astype('int')[i][0]
+          cv2.circle(viz, pt0, 2, (255, 0, 0), 2)
+          cv2.circle(viz, (pt1[0] + q_img.shape[1], pt1[1]), 2, (255, 0, 0), 2)
+          cv2.line(viz, pt0, (pt1[0] + q_img.shape[1], pt1[1]), (0, 0, 255), 1, cv2.LINE_AA)
+        good_match = np.count_nonzero(mask)
+        print(good, good_match)
+        cv2.putText(viz, 'sp sg match after ransac: '+str(good_match), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(viz, 'sp sg raw match: '+str(pts0.shape[0]), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2, cv2.LINE_AA)
   cv2.putText(viz, str(score), (10 + q_img.shape[1], 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2, cv2.LINE_AA)
-
   cv2.putText(viz, str(score1), (10 , 30 + q_img.shape[0]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2, cv2.LINE_AA)
-
   cv2.putText(viz, str(score2), (10 + q_img.shape[1], 30 + q_img.shape[0]), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2, cv2.LINE_AA)
-
-  cv2.putText(viz, 'query', (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2, cv2.LINE_AA)
-
-  # cv2.putText(viz, 'train0', (10 + q_img.shape[1], 50), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2, cv2.LINE_AA)
-
+  cv2.putText(viz, 'query', (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1.0, (0, 255, 0), 2, cv2.LINE_AA)
   cv2.imshow('pair', viz)
-
-  
-
   k = cv2.waitKey(100000)
-  print(k)
-
   if k == 113: #'q'
     break
