85eb2b30669afd5740e2f04c499871b257b86a4e
Guo, Fengyu <fyguo@telenav.cn>
2025-07-01 14:08:04 +0800

    add lightglue

--- image_local_feature_extractor_server.py
+++ image_local_feature_extractor_server.py
@@ -1,114 +1,190 @@
 import numpy as np
 import cv2
 from hloc.extractors import superpoint
-from hloc.matchers import superglue
+
 import torch
 import zmq
 import time
+import argparse
+import sys
 
-sp = superpoint.SuperPoint(superpoint.SuperPoint.default_conf).eval().to('cuda')
-sg = superglue.SuperGlue(superglue.SuperGlue.default_conf).eval().to('cuda')
+def get_args():
+  parser = argparse.ArgumentParser()
+  parser.add_argument(
+    '--match_model',
+    type=str,
+    default='superglue',
+    help='feature match model, now support super glue and light glue'
+  )
+  parser.add_argument(
+    '--lightglue_path',
+    type=str,
+    default='/home/guo/code/LightGlue-ONNX/lightglue',
+    help='path to lightglue model'
+  )
+  parser.add_argument(
+    '--model_weight',
+    type=str,
+    default='/home/guo/code/LightGlue-ONNX/weights/superpoint_lightglue.onnx',
+    help='path to weight of matcher. needed by lightglue matcher'
+  )
+  return parser
 
-context = zmq.Context()
-socket = context.socket(zmq.REP)
-zmq_port = "ipc:///tmp/zmq_spsg"
-socket.bind(zmq_port)
+class LocalFeatureServer:
+  def __init__(self, match_model, ext_path, model_weight, cam_model):
+    for p in ext_path:
+      sys.path.append(p)
+    self.method = match_model
+    if match_model == 'superglue':
+      from hloc.matchers import superglue
+      self.matcher = superglue.SuperGlue(superglue.SuperGlue.default_conf).eval().to('cuda')
+    elif match_model == 'lightglue':
+      from lightglue import LightGlue
+      conf = LightGlue.default_conf
+      conf['weights'] = model_weight
+      # TODO: how to config the lightglue model
+      self.matcher = LightGlue('superpoint').eval().to('cuda')
+    
+    self.extractor = superpoint.SuperPoint(superpoint.SuperPoint.default_conf).eval().to('cuda')
 
-print(f"image sp sg node start listen on port: {zmq_port}")
+    self.cam_model = cam_model
 
-from camera.camera_model import EquiDistCamera, RadTanCamera
-from dataset.mynteye_left import load_mynteye
+    self.last_img = None
+    self.last_tensor = None
+    self.last_desc = None
 
-cam_param = load_mynteye()
-cam_model = EquiDistCamera(np.array(cam_param['cam_mat'],dtype='float32'), np.array(cam_param['distortion'], dtype='float32'), np.array(cam_param['img_size_wh']))
+    print('local feature server init finished!')
+  def run(self):
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    zmq_port = "ipc:///tmp/zmq_spsg"
+    socket.bind(zmq_port)
+    print(f"image sp sg node start listen on port: {zmq_port}")
 
-last_img = None
-last_tensor = None
-last_desc = None
-
-while True:
-  msg = socket.recv_json()
-  print('got json from zmq: {}'.format(msg))
-  shape = msg["shape"]
-  dtype = np.dtype(msg["dtype"])
-  data = socket.recv()
-  
-  print('got image data from zmq {}'.format(len(data)))
-  start = time.time()
-  img = np.frombuffer(data, dtype=dtype).reshape(shape)
+    while True:
+      msg = socket.recv_json()
+      print('got json from zmq: {}'.format(msg))
+      shape = msg["shape"]
+      dtype = np.dtype(msg["dtype"])
+      data = socket.recv()
+      
+      print('got image data from zmq {}'.format(len(data)))
+      start = time.time()
+      img = np.frombuffer(data, dtype=dtype).reshape(shape)
 
 
-  q_tensor = torch.from_numpy(img).float() / 255.0
-  q_tensor = q_tensor.unsqueeze(0).unsqueeze(0)
-  # print(img_tensor.shape)
-  q_desc = sp({"image": q_tensor.to('cuda', non_blocking=True)})
-  # print(q_desc['keypoints'], q_desc['scores'], q_desc['descriptors'])
-  if last_img is None:
-    last_img = img
-    last_tensor = q_tensor
-    last_desc = q_desc
-    socket.send_string("got image")
-    continue
-  viz = np.hstack((img, last_img))
-  viz = cv2.cvtColor(viz, cv2.COLOR_GRAY2BGR)
-  print(img.shape)
-  print(img.shape, img.dtype)
-  # print(q_desc['keypoints'][0])
-  # print(q_desc.keys())
+      q_tensor = torch.from_numpy(img).float() / 255.0
+      q_tensor = q_tensor.unsqueeze(0).unsqueeze(0)
+      # print(img_tensor.shape)
+      q_desc = self.extractor({"image": q_tensor.to('cuda', non_blocking=True)})
+      print(q_desc['descriptors'][0].shape)
+      if self.last_img is None:
+        self.last_img = img
+        self.last_tensor = q_tensor
+        self.last_desc = q_desc
+        socket.send_string("got image")
+        continue
+      viz = np.hstack((img, self.last_img))
+      viz = cv2.cvtColor(viz, cv2.COLOR_GRAY2BGR)
+      print(img.shape)
+      print(img.shape, img.dtype)
+      # print(q_desc['keypoints'][0])
+      # print(q_desc.keys())
 
-  t_tensor = last_tensor
-  t_desc = last_desc
-  kps0 = q_desc['keypoints'][0].cpu().numpy()
-  kps1 = t_desc['keypoints'][0].cpu().numpy()
-  match_input = {
-    'image0': q_tensor,
-    'keypoints0': q_desc['keypoints'][0].unsqueeze(0),
-    'scores0': q_desc['scores'][0].unsqueeze(0),
-    'descriptors0': q_desc['descriptors'][0].unsqueeze(0),
-    'image1': t_tensor,
-    'keypoints1': t_desc['keypoints'][0].unsqueeze(0),
-    'scores1': t_desc['scores'][0].unsqueeze(0),
-    'descriptors1': t_desc['descriptors'][0].unsqueeze(0)
-  }
-  match = sg(match_input)
-  last_tensor = q_tensor
-  last_desc = q_desc
-  last_img = img
-  mt0 = match['matches0'].cpu().numpy()[0]
-  # print(mt0)
-  pts0 = []
-  pts1 = []
-  for idx1, idx2 in enumerate(mt0):
-    if idx2 == -1:
-      continue
-    pts0.append(kps0[idx1])
-    pts1.append(kps1[idx2])
-  raw_match = len(pts0)
-  pts0 = np.array(pts0, dtype='float32').reshape(-1, 1, 2)
-  pts1 = np.array(pts1, dtype='float32').reshape(-1, 1, 2)
-  print(pts0.shape)
-  if pts0.shape[0] > 0:
-    pts0_n = cam_model.undistort_points(pts0)
-    pts1_n = cam_model.undistort_points(pts1)
-    F, mask = cv2.findFundamentalMat(pts0_n, pts1_n, cv2.FM_RANSAC, ransacReprojThreshold=1.0, confidence=0.99, maxIters = 200)
-    good = 0
-    mask = np.squeeze(mask)
-    print(mask.shape)
-    good_match = np.count_nonzero(mask)
-    print('good match: {}, raw match: {}'.format(good_match, raw_match))
-    if good_match > 0:
-      for i, m in enumerate(mask.tolist()):
-        if m == 0:
+      t_tensor = self.last_tensor
+
+      null_img = np.zeros_like(img)
+      null_tensor = torch.from_numpy(null_img).float()
+      null_tensor = null_tensor.unsqueeze(0).unsqueeze(0)
+
+      t_desc = self.last_desc
+      kps0 = q_desc['keypoints'][0].cpu().numpy()
+      kps1 = t_desc['keypoints'][0].cpu().numpy()
+      if self.method == 'superglue':
+        match_input = {
+          'image0': null_tensor,
+          'keypoints0': q_desc['keypoints'][0].unsqueeze(0),
+          'scores0': q_desc['scores'][0].unsqueeze(0),
+          'descriptors0': q_desc['descriptors'][0].unsqueeze(0),
+          'image1': null_tensor,
+          'keypoints1': t_desc['keypoints'][0].unsqueeze(0),
+          'scores1': t_desc['scores'][0].unsqueeze(0),
+          'descriptors1': t_desc['descriptors'][0].unsqueeze(0)
+        }
+      elif self.method == 'lightglue':
+        match_input = {
+          'image0':{
+            'keypoints':q_desc['keypoints'][0].unsqueeze(0),
+            'descriptors':q_desc['descriptors'][0].transpose(1, 0).unsqueeze(0),
+            'image': null_tensor
+          },
+          'image1':{
+            'keypoints':t_desc['keypoints'][0].unsqueeze(0),
+            'descriptors':t_desc['descriptors'][0].transpose(1, 0).unsqueeze(0),
+            'image': null_tensor
+          }
+        }
+        # print(match_input['image0']['keypoints'].shape)
+        # print(match_input['image0']['descriptors'].shape)
+      
+      match = self.matcher(match_input)
+      self.last_tensor = q_tensor
+      self.last_desc = q_desc
+      self.last_img = img
+      mt0 = match['matches0'].cpu().numpy()[0]
+      # print(mt0)
+      pts0 = []
+      pts1 = []
+      for idx1, idx2 in enumerate(mt0):
+        if idx2 == -1:
           continue
-        good += 1
-        pt0 = pts0.astype('int')[i][0]
-        pt1 = pts1.astype('int')[i][0]
-        cv2.circle(viz, pt0, 2, (255, 0, 0), 2)
-        cv2.circle(viz, (pt1[0] + img.shape[1], pt1[1]), 2, (255, 0, 0), 2)
-        cv2.line(viz, pt0, (pt1[0] + img.shape[1], pt1[1]), (0, 0, 255), 1, cv2.LINE_AA)
+        pts0.append(kps0[idx1])
+        pts1.append(kps1[idx2])
+      raw_match = len(pts0)
+      pts0 = np.array(pts0, dtype='float32').reshape(-1, 1, 2)
+      pts1 = np.array(pts1, dtype='float32').reshape(-1, 1, 2)
+      print(pts0.shape)
+      if pts0.shape[0] > 0:
+        pts0_n = self.cam_model.undistort_points(pts0)
+        pts1_n = self.cam_model.undistort_points(pts1)
+        F, mask = cv2.findFundamentalMat(pts0_n, pts1_n, cv2.FM_RANSAC, ransacReprojThreshold=1.0, confidence=0.99, maxIters = 200)
+        good = 0
+        mask = np.squeeze(mask)
+        print(mask.shape)
+        good_match = np.count_nonzero(mask)
+        print('good match: {}, raw match: {}'.format(good_match, raw_match))
+        if good_match > 0:
+          for i, m in enumerate(mask.tolist()):
+            if m == 0:
+              continue
+            good += 1
+            pt0 = pts0.astype('int')[i][0]
+            pt1 = pts1.astype('int')[i][0]
+            cv2.circle(viz, pt0, 2, (255, 0, 0), 2)
+            cv2.circle(viz, (pt1[0] + img.shape[1], pt1[1]), 2, (255, 0, 0), 2)
+            cv2.line(viz, pt0, (pt1[0] + img.shape[1], pt1[1]), (0, 0, 255), 1, cv2.LINE_AA)
 
-    cv2.imshow('match', viz)
-    k = cv2.waitKey(1)
-    if k == 'q':
-      break
-  socket.send_string("got image")
+        cv2.imshow('match', viz)
+        k = cv2.waitKey(1)
+        if k == 'q':
+          break
+      socket.send_string("got image")
+
+def main():
+
+  parser = get_args()
+
+  args = parser.parse_args()
+
+  from camera.camera_model import EquiDistCamera, RadTanCamera
+  from dataset.mynteye_left import load_mynteye
+
+  cam_param = load_mynteye()
+  cam_model = EquiDistCamera(np.array(cam_param['cam_mat'],dtype='float32'), np.array(cam_param['distortion'], dtype='float32'), np.array(cam_param['img_size_wh']))
+
+  server = LocalFeatureServer(args.match_model, [args.lightglue_path], args.model_weight, cam_model)
+
+  server.run()
+
+if __name__ == '__main__':
+  main()
